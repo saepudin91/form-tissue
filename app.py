20e95ee965064d9b4fc905e69f9a70bf8fe9f1e2
saepudin91 <119606890+saepudin91@users.noreply.github.com>
2025-08-07 17:22:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds_dict = st.secrets["gcp_service_account"]
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client = gspread.authorize(creds)
+
+# Contoh buka spreadsheet
+sheet = client.open("Nama Spreadsheet Kamu").sheet1
+data = sheet.get_all_records()
+st.write(data)
